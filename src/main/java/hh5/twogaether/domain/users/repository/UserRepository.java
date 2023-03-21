6ee6b3ce2ea08f70b2590f72c927b6f8d94ee455
midcondria <hyukkind@naver.com>
2023-03-21 15:15:21 +0900

    #144 이메일 인증에 프로젝션 적용

--- src/main/java/hh5/twogaether/domain/users/repository/UserRepository.java
+++ src/main/java/hh5/twogaether/domain/users/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package hh5.twogaether.domain.users.repository;
 
+import hh5.twogaether.domain.users.dto.EmailOnlyDto;
 import hh5.twogaether.domain.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,10 +15,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select u from User u where u.isDelete = false ")
     List<User> findAllNotDeletedUser();
+
     @Query("select u from User u where u.id = :id and u.isDelete = false")
     User findNotDeletedUserById(@Param("id") Long id);
     Optional<User> findByUsername(String username);
-    Optional<User> findByNickname(String username);
+    @Query("select u.username from User u where u.username =:email")
+    Optional<EmailOnlyDto> findByEmail(@Param("email") String email);
     Optional<User> findById(Long id);
     List<User> findAll();
 
