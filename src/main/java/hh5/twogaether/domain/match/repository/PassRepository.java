06c3fb0f842e53ceca4a6740418a6f56d5570646
midcondria <hyukkind@naver.com>
2023-03-21 15:24:33 +0900

    #85 PassRepository 수정 싫어요 목록 리셋 메서드 추가

--- src/main/java/hh5/twogaether/domain/match/repository/PassRepository.java
+++ src/main/java/hh5/twogaether/domain/match/repository/PassRepository.java
@@ -2,11 +2,16 @@ package hh5.twogaether.domain.match.repository;
 
 import hh5.twogaether.domain.match.entity.Pass;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PassRepository extends JpaRepository<Pass, Long> {
 
-    @Query("select p from Pass p where p.createdBy = :createdBy and p.dogId =:dogId")
+    @Query("select p from Pass p where p.createdBy =:createdBy and p.dogId =:dogId")
     Pass findByCreatedByAndDogId(@Param("createdBy") Long createdBy, @Param("dogId") Long dogId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Pass p where p.isLoved = false")
+    void deleteAllisNotLoved();
 }
