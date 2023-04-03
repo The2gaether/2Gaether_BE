e0d08f77dc77c015af63682b73be16a234765718
midcondria <hyukkind@naver.com>
2023-04-03 11:49:06 +0900

    #152 매칭페이지 로직 리팩토링

--- src/main/java/hh5/twogaether/domain/match/service/MatchService.java
+++ src/main/java/hh5/twogaether/domain/match/service/MatchService.java
@@ -30,53 +30,44 @@ public class MatchService {
     private final MatchQueryRepository matchQueryRepository;
 
     @Transactional
-    public MatchDogResponseDto getMatches(User me) {
-
-        //내 개가 있을때 로직 시작
-        isExistMyDog(me.getId());
-
-        //기존에 저장된 리스트가 있을 시 기존 정보 사용, 없을 시 새 리스트 저장
-        if (matchRepository.findByCreatedBy(me.getId()).size() == 0) {
-            createMatches(me);
-        }
-        //좋아요, 싫어요 하지 않은 목록 불러와서 셔플
-        List<Match> matches = getAndShuffleMatches(me.getId());
-
-        //다 넘겨서 남은 매칭상대가 없는 경우 Matches 다 지우고 다시 갱신
-        if (matches.size() == 0) {
+    public MatchDogResponseDto getRandomMatchedOne(User me) {
+        checkIsExistMyDog(me.getId());
+        List<Match> matches = matchRepository.findAllNotPassedByCreatedBy(me.getId());
+        if (matches.isEmpty()) {
             matchRepository.deleteAllByCreatedBy(me.getId());
             createMatches(me);
-            matches = getAndShuffleMatches(me.getId());
         }
-        //예외처리
-        if (matches.size() == 0) {
-            throw new IllegalArgumentException("매칭 상대가 없습니다");
-        }
-        //matches 에서 1개 뽑아서 보여줌
-        int distance = matches.get(0).getDistance();
-        Long dogId = matches.get(0).getDogId();
-        Dog dog = dogRepository.findById(dogId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 유저입니다.")
-        );
-        return new MatchDogResponseDto(dog, distance);
+        matches = getShuffledMatches(me.getId());
+        return getOne(matches);
     }
 
-    private void isExistMyDog(Long id) {
+    private void checkIsExistMyDog(Long id) {
         List<Dog> myDogs = dogRepository.findAllNotDeletedByCreatedByDog(id);
         if (myDogs.isEmpty()) {
             throw new IllegalArgumentException("강아지를 추가 후 이용해주세요");
         }
     }
 
-    //좋아요, 싫어요 하지 않은 목록 불러와서 셔플
-    private List<Match> getAndShuffleMatches(Long id) {
+    private List<Match> getShuffledMatches(Long id) {
         List<Match> matches = matchRepository.findAllNotPassedByCreatedBy(id);
         shuffle(matches);
         log.info("size = {}",matches.size());
+        if (matches.isEmpty()) {
+            throw new IllegalArgumentException("매칭 상대가 없습니다");
+        }
         return matches;
     }
 
-    //좋아요
+    private MatchDogResponseDto getOne(List<Match> matches) {
+        int distance = matches.get(0).getDistance();
+        Long dogId = matches.get(0).getDogId();
+        Dog dog = dogRepository.findById(dogId).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 유저입니다.")
+        );
+        MatchDogResponseDto result = new MatchDogResponseDto(dog, distance);
+        return result;
+    }
+
     @Transactional
     public void loveUser(Long dogId, Long myId) {
         Match opponent = matchRepository.findByDogIdAndCreatedBy(dogId, myId);
@@ -87,7 +78,6 @@ public class MatchService {
         opponent.passUser();
     }
 
-    //싫어요
     @Transactional
     public void passUser(Long dogId, Long myId) {
         Match opponent = matchRepository.findByDogIdAndCreatedBy(dogId, myId);
@@ -98,7 +88,6 @@ public class MatchService {
         opponent.passUser();
     }
 
-    //Match 리스트 저장
     @Transactional
     public void createMatches(User me) {
         List<MatchDto> matchDtos = matchQueryRepository.findAllNotDeletedAndNotPassedDog(me.getId());
@@ -122,7 +111,6 @@ public class MatchService {
         return earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
     }
 
-    //거리 반올림하기
     private int roundDistance(double distance) {
         if (Math.round(distance) < 1) {
             return 1;
